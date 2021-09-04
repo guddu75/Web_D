f42eb08f8234a6117473fc2aabfb0773015e39f5
Debapriya Majumder <guddumajumder75@gmail.com>
2021-09-04 17:41:50 +0530

    mongoose crud operation

--- fruitProject/app.js
+++ fruitProject/app.js
@@ -1,37 +1,106 @@
 
-const MongoClient = require('mongodb').MongoClient;
+const mongoose = require("mongoose");
 
-const assert = require('assert');
+mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser : true});
 
-const url = "mongodb://localhost:27017";
-
-const dbName = "fruitsDB" ;
-
-const client = new MongoClient(url , {useNewUrlParser : true});
-
-client.connect(function(err){
-    assert.equal(null,err);
-    console.log("Connected successfully to server");
-
-    const db = client.db(dbName);
-
-    client.close();
-
+const fruitSchema = new mongoose.Schema({
+    name : {
+        type : String,
+        required : true ,
+    },
+    rating : {
+        type : Number,
+        min : 1 ,
+        max : 10,
+    },
+    review : String
 });
 
-const insertDocuments = function(db, callback) {
+const Fruit = mongoose.model("Fruit",fruitSchema);
 
-    const collection = db.collection('fruits');
+const fruit = new Fruit({
+    name : "Pineapple",
+    rating : 7,
+    review : "Best fruit !"
+});
 
-    collection.insertMany([
+fruit.save();
 
-    ],function(err,result){
-        assert.equal(err, result);
-        assert.equal(3, result.result.n);
-        assert.equal(3,result.ops.length);
-    })
+const personSchema = new mongoose.Schema({
+    name : String,
+    age : Number,
+    favouriteFruit : fruitSchema
+});
 
-}
+const People = mongoose.model("People",personSchema);
+
+const person = new People({
+    name : "Debapriya Majumder",
+    age : 21 , 
+    // favouriteFruit : fruit
+});
+
+// person.save();
+
+const mango = new Fruit({
+    name : "Mango",
+    rating : 10,
+    review : "Best fruit !"
+});
+
+const banana = new Fruit({
+    name: "Banana",
+    rating : 9 ,
+    review : "healthy af !"
+})
+
+const lemon = new Fruit({
+    name : "Lemon",
+    rating : 6,
+    review : "Sour "
+});
+
+// Fruit.insertMany([mango, banana , lemon], function(err){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("Success");
+//     }
+// })
+
+
+
+People.updateOne({name : "Debapriya Majumder"}, {favouriteFruit : fruit} ,function(err){
+    if(err){
+        console.log(err);
+    }else{
+        console.log("Updated");
+    }
+});
+
+// Fruit.deleteOne({name: "Peach"}, function(err){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         console.log("deletd!");
+//     }
+// });
+
+// Fruit.find(function(err,fruits){
+//     if(err){
+//         console.log(err);
+//     }else{
+//         // console.log(fruits);
+//         mongoose.connection.close();
+//         fruits.forEach(function(fruit){
+//             console.log(fruit.name);
+//         });
+//     }
+// });
+
+
+
+
 
 
 
